265e183e9461ca2f8389479f2fdd6bf691675d7a
Sergio Blanco <108953777+Serblaor@users.noreply.github.com>
2023-07-23 14:05:13 -0500

    Primera prueba

---
+++ CirculoApp/CirculoApp/Circulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirculoApp
+{
+    internal class Circulo
+    {
+        private double radio;
+
+        public double Radio
+        {
+            get { return radio; }
+            set { radio = value; }
+        }
+
+        public double Area
+        {
+            get { return Math.PI * radio * radio; }
+        }
+
+        public double Perimetro
+        {
+            get { return 2 * Math.PI * radio; }
+        }
+    }
+}
---
+++ CirculoApp/CirculoApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CirculoApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para calcular el área y el perímetro de un círculo");
+
+            // Solicitar al usuario ingresar el radio del círculo
+            Console.Write("Ingrese el radio del círculo: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+
+            // Crear una instancia de la clase Circulo
+            Circulo circulo = new Circulo();
+
+            // Asignar el radio ingresado a la propiedad correspondiente
+            circulo.Radio = radio;
+
+            // Mostrar el área y el perímetro del círculo
+            Console.WriteLine("El área del círculo es: " + circulo.Area);
+            Console.WriteLine("El perímetro del círculo es: " + circulo.Perimetro);
+
+            Console.ReadLine();
+        }
+    }
+}
